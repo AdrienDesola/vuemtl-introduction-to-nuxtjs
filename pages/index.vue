db247092ef327d1439212e4f6451d0b39bf38ad7
Adrien Desola <adriendesola@gmail.com>
2019-10-30 15:51:10 -0400

    step 2 - index

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
     <h1> Movies </h1>
+    <b-row align-h="around">
+      <b-col v-for="(movie, index) in movies" :key="`movies-${index}`" cols="6" xl="4">
+        <Movie
+          :id="movie.id"
+          :title="movie.title"
+          :description="movie.description"
+          :poster="movie.poster"
+        />
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
-
+import Movie from '~/components/Movie'
 export default {
+  components: {
+    Movie
+  },
   head () {
     return {
       title: 'Movies'
+    }
+  },
+  async asyncData ({ $axios }) {
+    const response = await $axios.$get('/api/tmdb/discover/movie')
+    const movies = response.results.map(r => ({
+      id: r.id,
+      title: r.title,
+      poster: `/cdn/tmdb${r.poster_path}`,
+      description: r.overview
+    }))
+    return {
+      movies
     }
   }
 }
